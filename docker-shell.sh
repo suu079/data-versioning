3b72dad5bcd954aa2b90f1e4292dc1b6a3be0e87
Susan Urban <suu079@g.harvard.edu>
2025-03-29 12:47:02 -0400

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,8 +4,8 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="cheese-app-data-demo-susanu"
+export GCP_PROJECT="stalwart-micron-455214-r6"
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
